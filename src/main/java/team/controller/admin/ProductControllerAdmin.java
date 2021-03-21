1c3f0c7ff4c5815945ebd2513582f19eb495b497
AngelikiPan <74975215+AngelikiPan@users.noreply.github.com>
2021-03-21 17:40:33 +0200

    CRUD for admin/product ok

--- src/main/java/team/controller/admin/ProductControllerAdmin.java
+++ src/main/java/team/controller/admin/ProductControllerAdmin.java
@@ -17,14 +17,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import team.entity.Brand;
 import team.entity.Category;
 import team.entity.Color;
 import team.entity.Gender;
 import team.entity.Sizes;
+import team.entity.User;
 import team.service.BrandService;
 import team.service.CategoryService;
 import team.service.ColorService;
@@ -36,65 +40,65 @@ import team.service.SizesService;
  *
  * @author tasos
  */
-
 @RequestMapping("/admin/product")
 @Controller
 public class ProductControllerAdmin {
-    
+
     @Autowired
     ProductRepo productRepo;
-    
+
     @Autowired
     BrandService brandService;
-    
+
     @Autowired
     CategoryService categoryService;
-    
+
     @Autowired
     GenderService genderService;
-    
+
     @Autowired
     ProductService productService;
-    
+
     @Autowired
     SizesService sizesService;
-    
+
     @Autowired
     ColorService colorService;
-    
-     @ModelAttribute("brands")
+
+    @ModelAttribute("brands")
     public List<Brand> getBrands() {
         return brandService.findAll();
     }
+
     @ModelAttribute("categories")
     public List<Category> getCategories() {
         return categoryService.findAll();
     }
-    
+
     @ModelAttribute("genders")
     public List<Gender> getGenders() {
         return genderService.findAll();
     }
-    
+
     @ModelAttribute("sizes")
-    public List<Sizes> getSizes(){
+    public List<Sizes> getSizes() {
         return sizesService.findAll();
     }
-    
+
     @ModelAttribute("colors")
-    public List<Color> getColors(){
+    public List<Color> getColors() {
         return colorService.findAll();
     }
-    
+
     @GetMapping
-    public String showProducts(Model model){
+    public String showProducts(Model model) {
         List<Product> listOfProducts = productRepo.findAll();
         model.addAttribute("listOfProducts", listOfProducts);
         return "Admin/productCRUD";
     }
     // =================CREATE METHODS============================
-    
- @RequestMapping(value = "/create", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
     public String showForm(@ModelAttribute("product") Product product) {
         return "Admin/productFormCREATE";
     }
@@ -108,13 +112,52 @@ public class ProductControllerAdmin {
             return "Admin/productFormCREATE";
         }
         productService.createProduct(product);
-        
-        String minima = "Product " + product.getTitle()+ " successfully created!!";
+
+        String minima = "Product " + product.getTitle() + " successfully created!!";
         attributes.addFlashAttribute("message", minima);
         return "redirect:/admin/product";
-    }   
-    
-     @ExceptionHandler(DataIntegrityViolationException.class)
+    }
+
+
+    //===========================================UPDATE===============================================
+    @GetMapping("/update/{productid}")
+    public String update(
+            Product product,
+            @PathVariable(value = "productid") int productid, Model model) {
+
+        Product productToUpdate = productRepo.findById(productid).get();
+
+        model.addAttribute("product", productToUpdate);
+        return "Admin/productFormUPDATE";
+    }
+
+    @PostMapping("/update")
+    public String update(@Valid @ModelAttribute("product") Product product, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "Admin/productFormUPDATE";
+        }
+
+        productRepo.save(product);
+
+        String minima = "Product updated successfully!!";
+        attributes.addFlashAttribute("message", minima);
+        return "redirect:/admin/product";
+    }
+        // =============================DELETE=====================================    
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") int id, RedirectAttributes attributes) {
+        String minima;
+
+        productRepo.deleteById(id);
+
+        minima = "Product deleted successfully!";
+        attributes.addFlashAttribute("message", minima);
+        return "redirect:/admin/product";
+    }
+
+    //===============================EXCEPTIONS=======================================
+    @ExceptionHandler(DataIntegrityViolationException.class)
     public String handleDataIntegrityViolationException(RedirectAttributes attributes) {
         String minima = "Could not commit transaction!!";
         attributes.addFlashAttribute("message", minima);
@@ -122,4 +165,5 @@ public class ProductControllerAdmin {
     }
     
     
+
 }
